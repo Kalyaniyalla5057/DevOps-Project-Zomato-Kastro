6c8edf94ca1ca6012be4b789ddf2052276ca60bc
mahirajula123 <mahirajula166@gmail.com>
2026-08-24 20:42:21 +0530

    Developer 3 update App component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import CTA from "./components/CTA/CTA";
 import Cities from "./components/Cities/Cities";
 import Collection from "./components/Collections/Collection";
 import Card from "./components/Card/Card";
-
+ 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Card />
-      <Collection />
-      <Cities />
-      <CTA />
-      <AccContainer />
-      <Footer />
-    </div>
+<div className="App">
+<Header />
+<h2>Developer 3 - Restaurant Collection Update</h2>
+<Card />
+<Collection />
+<Cities />
+<CTA />
+<AccContainer />
+<Footer />
+</div>
   );
 }
-
+ 
 export default App;
