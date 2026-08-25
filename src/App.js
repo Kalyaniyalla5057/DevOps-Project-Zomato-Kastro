792cb8ceae77668fec878432cf741057db6a31d7
Kalyani Yalla <yallakalyani2003@gmail.com>
2026-08-25 17:58:08 +0530

    Introduce production bug for rollback demonstration

--- src/App.js
+++ src/App.js
@@ -6,31 +6,33 @@ import CTA from "./components/CTA/CTA";
 import Cities from "./components/Cities/Cities";
 import Collection from "./components/Collections/Collection";
 import Card from "./components/Card/Card";
- 
+
 function App() {
   return (
-<div className="App">
-<Header />
+    <div className="App">
+      <Header />
 
-<Card />
-
- 
-      <h2>Developer 1 - Header and Navigation Update</h2>
-
-<h2>Developer 3 - Restaurant Collection Update</h2>
-
-<h2>Developer 2 - Food Discovery Update</h2>
-<h2>Developer 4 - Restaurant experience Update</h2>
+      <h2>🚨 PRODUCTION BUG - Broken Feature</h2>
 
       <Card />
 
-<Collection />
-<Cities />
-<CTA />
-<AccContainer />
-<Footer />
-</div>
+      <h2>Developer 1 - Header and Navigation Update</h2>
+
+      <h2>Developer 3 - Restaurant Collection Update</h2>
+
+      <h2>Developer 2 - Food Discovery Update</h2>
+
+      <h2>Developer 4 - Restaurant experience Update</h2>
+
+      <Card />
+
+      <Collection />
+      <Cities />
+      <CTA />
+      <AccContainer />
+      <Footer />
+    </div>
   );
 }
- 
+
 export default App;
